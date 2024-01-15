6a58943942e66527c18c00425087a65d264fff4c
jobayed003 <jobayed803@gmail.com>
2024-01-15 21:19:05 +0600

    added session and token manager

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import authConfig from '@/auth.config';
+import { getUserById } from '@/data/user';
 import { db } from '@/lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { UserRole } from '@prisma/client';
 import NextAuth from 'next-auth';
 
 export const {
@@ -9,6 +11,29 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, user, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+
+    async jwt({ token, user, profile }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
